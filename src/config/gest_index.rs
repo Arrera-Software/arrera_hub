9d606543cb341ebbf730ce0f553cb155700be3de
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-03-02 19:02:40 +0100

    Creation de la fonction pour enregistrer l'index en local

--- src/config/gest_index.rs
+++ src/config/gest_index.rs
@@ -1,12 +1,49 @@
+use std::fs;
+use std::path::PathBuf;
+use std::sync::Mutex;
 use crate::config::dect_os;
-pub fn save_index(content : &str) -> bool {
-    let os = dect_os();
+use dirs;
+
+static PATH_INDEX : Mutex<String> = Mutex::new(String::new());
+
+fn write_value(val: &str) {
+    let mut data = PATH_INDEX.lock().unwrap();
+    *data = val.to_string();
+}
+
+fn read_value() -> String {
+    let data = PATH_INDEX.lock().unwrap();
+    data.clone()
+}
+
+pub fn save_index(content: &str) -> bool {
+    let os: i32 = dect_os();
+
+    let mut dir = match dirs::home_dir() {
+        Some(path) => path,
+        None => return false,
+    };
 
     if os == 1 {
-
-    }else if os {
-
+        // WINDOWS
+        dir.push("AppData");
+        dir.push("Roaming");
+        dir.push("arrera-hub");
+    } else if os == 2 || os == 3 {
+        // LINUX & MAC
+        dir.push(".config");
+        dir.push("arrera-hub");
+    } else {
+        return false;
     }
 
-    false
+    dir.push("index.json");
+
+    if let Some(parent) = dir.parent() {
+        if fs::create_dir_all(parent).is_err() {
+            return false;
+        }
+    }
+
+    fs::write(dir, content).is_ok()
 }
--- src/depots/index.rs
+++ src/depots/index.rs
@@ -1,9 +1,11 @@
 use reqwest::blocking::get;
+use crate::config::gest_index::save_index;
+
 pub fn load_depots() -> Result<(), Box<dyn std::error::Error>> {
     // Dois recuprer le contenu de l'index et le stoker dans un fichier json dans .config (Pour mac os et linux)
     let content = reqwest::blocking::get(
         "https://raw.githubusercontent.com/Arrera-Software/distribution/refs/heads/main/index.json")
         ?.text()?;
-    
+    save_index(&*content);
     Ok(())
 }
