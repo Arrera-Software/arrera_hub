cb0e1087b168e0d1c02193ccbafe5fa13aefbde4
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2026-03-19 13:26:33 +0100

    Creation de la struture de mon module Arrera Hub

---
+++ src/arrera_hub/arrera_hub.rs
@@ -0,0 +1,23 @@
+impl arrera_hub {
+    pub fn new() -> Self{
+    }
+
+    pub fn update(&self){
+
+    }
+
+    pub fn update_all_soft(&self){
+
+    }
+    pub fn get_soft_available(&self){
+
+    }
+
+    pub fn get_soft_installed(&self){
+
+    }
+
+    pub fn install_soft(&self) {
+
+    }
+}
