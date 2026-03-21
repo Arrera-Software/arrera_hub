65d89cd1e2eb6ea354212c18722b4861faad71ad
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-03-21 14:18:53 +0100

    Ajout de la partie logiciel disponible

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,22 @@ async fn main() {
 
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
-        if args[1] == "help"{
-            println!("Arrera Hub\n- help \n- about")
-        }else if args[1] == "about" {
+        if args[1] == "help" {
+            println!("Arrera Hub\n- help \n- about\n- available\n- update")
+        } else if args[1] == "about" {
             println!("Arrera Hub by Arrera Software");
             println!("Version I2026-0.00");
             println!("Copyright Arrera-Software by Baptiste P 2023-2026");
+        } else if args[1] == "available" {
+            let list_soft = hub.get_soft_available().unwrap();
+            println!("Logiciels disponibles chez Arrera :");
+            for software in list_soft {
+                println!("- {}", software.name);
+            }
         }
+
+    } else {
+        println!("Arrera Hub\n- help \n- about\n - available\n - update")
     }
 
 
