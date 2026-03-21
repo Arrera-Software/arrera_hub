0a9db31461e13c75df04e93fba6b13b26c2d4cc6
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-03-21 11:21:03 +0100

    Debut de la creation de l'outil en ligne de commande

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 mod hub;
 mod depots;
 mod config;
@@ -5,5 +7,19 @@ mod config;
 
 #[tokio::main]
 async fn main() {
+
     let hub = hub::arrera_hub::ArreraHub::new().await.unwrap();
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        if args[1] == "help"{
+            println!("Arrera Hub\n- help \n- about")
+        }else if args[1] == "about" {
+            println!("Arrera Hub by Arrera Software");
+            println!("Version I2026-0.00");
+            println!("Copyright Arrera-Software by Baptiste P 2023-2026");
+        }
+    }
+
+
 }
