ee356cf1b116963d548a3e1f487d88def5b7b85b
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-04-25 21:53:11 +0200

    Creation du squellette de l'objet

--- core/hub.cpp
+++ core/hub.cpp
@@ -1,11 +1,46 @@
 #include "hub.h"
 
-Hub::Hub()
+Hub::Hub(QString url)
 {
-    //ctor
+
 }
 
 Hub::~Hub()
 {
-    //dtor
+
+}
+
+bool Hub::update_depots()
+{
+    return true;
+}
+
+QStringList Hub::get_software_with_update()
+{
+    return [];
+}
+
+bool Hub::get_software_udpate(QString soft)
+{
+    return true;
+}
+
+bool Hub::install_software(QString soft)
+{
+    return true;
+}
+
+bool Hub::uninstall_software(QString soft)
+{
+    return true;
+}
+
+QStringList Hub::get_soft_available()
+{
+    return [];
+}
+
+QStringList Hub::get_soft_installed()
+{
+    return [];
 }
--- core/hub.h
+++ core/hub.h
@@ -1,10 +1,24 @@
-
+#include <QString>
 
 class Hub
 {
     public:
-        Hub();
+        Hub(QString url);
         virtual ~Hub();
+        // Gestion de depots
+        bool update_depots();
+
+        // Mise a jour des logiel
+        QStringList get_software_with_update();
+        bool get_software_udpate(QString soft);
+
+        //Installation et desinstallation des logiciel
+        bool install_software(QString soft);
+        bool uninstall_software(QString soft);
+
+        // Methode pour avoir sur les logicel
+        QStringList get_soft_available();
+        QStringList get_soft_installed();
 
     protected:
 
